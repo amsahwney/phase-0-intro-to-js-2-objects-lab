59c6a638f0f1e38b8105a82c950659ac21e649bf
Amrita <amritak.saw@gmail.com>
2024-10-02 22:38:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+
+// creating employee object
+const employee = {
+    A : 123,
+    B : 456,
+    C : 789, 
+}
+
+// creating function 1
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+      }; 
+}
+// okay 2 tests passing now
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+    return obj 
+}
+// on to the 4th test yay
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj}
+    delete newObj[key]
+    return newObj
+}
+// passing 5 tests!
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    return obj
+}
+
+// DONE :) 
